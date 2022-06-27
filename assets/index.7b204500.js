9b83a5ccccfb0a50f29692e75190a992756df803
ioancw <ioan.williams@icloud.com>
2022-06-27 09:25:54 +0100

    Updates

--- assets/index.7b204500.js
+++ assets/index.7b204500.js
@@ -98,7 +98,6 @@ import{y as dr,n as hr}from"./vendor.299b0ab0.js";const fr=function(){const s=do
         <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
         </svg>
         </div>
-        <div class="flex mb-2"></div>
         <div>
         <svg @click=${i(new Se(1))} xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
