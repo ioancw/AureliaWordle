028a72b4e5587c8fdecbe1dbe4c36608cedfcf23
ioancw <ioan.williams@icloud.com>
2023-09-20 12:31:33 +0100

    Updates

--- assets/index.2db29a5e.js
+++ assets/index.2db29a5e.js
@@ -187,7 +187,7 @@ const so=function(){const e=document.createElement("link").relList;if(e&&e.suppo
             ${h(q(5,n.Guesses))}
             </div>
             </div>
-            <div class="absolute inset-x-0 bottom-0">
+            <div>
             <div class="flex justify-center mb-1.5">
             ${se(f,js.Top)}
             </div>
